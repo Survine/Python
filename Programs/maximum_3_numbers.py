d92f0f576e5063353c28d38f809d79a3144fdf5d
Survine <hridaybardhan18@gmail.com>
2024-10-23 22:32:01 +0530

    task 2

---
+++ Programs/maximum_3_numbers.py
@@ -0,0 +1,4 @@
+a = int(input("Enter the 1st number: "))
+b = int(input("Enter the 2nd number: "))
+c = int(input("Enter the 3rd number: "))
+print(f"The maximum of {a}, {b} and {c} is {max(a, b, c)}")
